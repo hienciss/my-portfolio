068935fd0c88b190c671df8b52f0bf5245c8cb5b
HiểnLê <lengochien29072k5@gmail.com>
2025-11-21 09:28:04 +0700

    Add DOMContentLoaded event listener to script.js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của Lê Ngọc Hiển đã load xong!');
+    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
+});
